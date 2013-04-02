fa47825a7d34dd508bb73093be3dba544a789db6
Henrique Riccio <henrique.riccio@iconware.com.br>
2013-04-01 23:22:10 -0300

    Importacao

---
+++ tests/EasyArchitecture.Plugins.Ninject.Tests/ContainerTest.cs
@@ -0,0 +1,24 @@
+﻿using System.Reflection;
+using EasyArchitecture.Core;
+using EasyArchitecture.Plugins.Tests.IoC;
+using NUnit.Framework;
+
+namespace EasyArchitecture.Plugins.Ninject.Tests
+{
+    [TestFixture]
+    [Ignore("POC")]
+    public class ContainerTest : MinimalContainerTest
+    {
+        [SetUp]
+        public override void SetUp()
+        {
+            var plugin = new NinjectPlugin();
+
+            PluginInspector pluginInspector;
+            plugin.Configure(new PluginConfiguration(null, null, null, Assembly.GetExecutingAssembly()),
+                             out pluginInspector);
+
+            Container = plugin.GetInstance();
+        }
+    }
+}
